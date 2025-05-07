a22190a605f19d3cf87348481cc7454627b49476
panejii <faneziaja@gmail.com>
2025-05-07 22:14:58 +0700

    menyesuaikan main dan test insert node root

--- main.c
+++ main.c
@@ -1,59 +1,68 @@
-    #include <stdio.h>
-    #include "BinSTree.h"
+        #include <stdio.h>
+        #include "BinSTree.h"
 
-    int main(){
+        int main(){
 
-        int PilihMenu;
-        BinTree tree;
+            int PilihMenu;
+            BinTree tree = Nil;
+            char InputUser;
 
-            while(1){
-                printf("\n==========================\n");
-                printf(
-                    " 1. Insert Node\n"
-                    " 2. Print Tree\n"
-                    " 3. Traversal PreOrder\n"
-                    " 4. Traversal InOrder\n"
-                    " 5. Traversal PostOrder\n"
-                    " 6. Traversal Level Order\n"
-                    " 7. Search Node Tree\n"
-                    " 8. Jumlah Daun/Leaf Tree\n"
-                    " 9. Mencari kedalaman Tree\n"
-                    "10. Membandingkan 2 Node Tree\n"
-                    "11. Exit\n");
-                printf("Pilih Menu: ");
-                scanf("%d", &PilihMenu);
+                while(1){
+                    printf("\n==========================\n");
+                    printf(
+                        " 1. Insert Node\n"
+                        " 2. Print Tree\n"
+                        " 3. Traversal PreOrder\n"
+                        " 4. Traversal InOrder\n"
+                        " 5. Traversal PostOrder\n"
+                        " 6. Traversal Level Order\n"
+                        " 7. Search Node Tree\n"
+                        " 8. Jumlah Daun/Leaf Tree\n"
+                        " 9. Mencari kedalaman Tree\n"
+                        "10. Membandingkan 2 Node Tree\n"
+                        "11. Exit\n");
+                    printf("Pilih Menu: ");
+                    scanf("%d", &PilihMenu);
 
-                switch (PilihMenu)
-                {
-                case 1:
+                    switch (PilihMenu)
+                    {
+                    case 1:
+                        if(tree == Nil){
+                            printf("Masukan Node root : ");
+                            scanf(" %c",&InputUser);
+                            getchar();
 
-                    break;
-                case 2:
-                    PrintTree(,0);
-                    break;
-                case 3:
+                            MakeTree(InputUser,Nil,Nil,&tree);
+                        }else{
+                            printf("tree tidak ada");
+                        }
+                        break;
+                    case 2:
+                        PrintTree(tree,0);
+                        break;
+                    case 3:
 
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    return 0;
-                    break;
-                default:
-                    break;
-                }
-            }  
-    }
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        return 0;
+                        break;
+                    default:
+                        break;
+                    }
+                }  
+        }
 

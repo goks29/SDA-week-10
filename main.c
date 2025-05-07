428904a4963ed0c5d930bc6266c998f3bfb42f1b
panejii <faneziaja@gmail.com>
2025-05-07 20:07:51 +0700

    menu main awal

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "BinSTree.h"
+
+int main(){
+    int PilihMenu;
+    
+
+    while(1){
+        while(1){
+            printf("\n==========================\n");
+            printf(" 1. Traversal PreOrder\n"
+                   " 2. Traversal InOrder\n"
+                   " 3. Traversal PostOrder\n"
+                   " 4. Traversal Level Order\n"
+                   " 5. Print Tree\n"
+                   " 6. Search Node Tree\n"
+                   " 7. Jumlah Daun/Leaf\n"
+                   " 8. Jumlah Node\n"
+                   " 9. Mencari Level Node Tree\n"
+                   " 10. Kedalaman Tree\n"
+                   "11. Membandingkan 2 Node Tree\n"
+                   "12. Exit\n");
+            printf("Pilih Menu: ");
+            scanf("%d", &PilihMenu);
+    }
+
+}
